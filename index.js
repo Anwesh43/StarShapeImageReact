563004e4dddd09c61448b976a3e4dd3b0447bcdf
Anwesh43 <anweshthecool0@gmail.com>
2017-08-26 15:03:00 +0530

    using StarShapeImageComponent in Index Component

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
+import StarShapeImageSourceComponent from './StarShapeImageComponent'
 class Index extends Component {
     render() {
         return (<div>
+                    <StarShapeImageSourceComponent image="stp.jpg" color="#0097A7"/>
                 </div>)
     }
 }
