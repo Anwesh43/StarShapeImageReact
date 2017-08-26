919bdbb895419c3b097c472cfb6b881273f67e7a
Anwesh43 <anweshthecool0@gmail.com>
2017-08-26 15:08:27 +0530

    adding minor fixes

--- StarShapeImageComponent.js
+++ StarShapeImageComponent.js
@@ -5,7 +5,7 @@ class StarShapeImageComponent extends Component {
     }
     render() {
         return (<div>
-                    <img src={this.props.image} onclick={this.props.onclick}/>
+                    <img src={this.props.image} onClick={this.props.onclick}/>
                 </div>)
     }
 }
@@ -36,7 +36,7 @@ export default class StarShapeImageSourceComponent extends Component {
                     clearInterval(interval)
                     this.setState({animated:false})
                 }
-            })
+            },40)
         }
     }
     drawOverStar(image,color,scale=0) {
@@ -45,8 +45,8 @@ export default class StarShapeImageSourceComponent extends Component {
             for(var i=0;i<6;i++) {
                 var deg = i*60
                 var x = r1*Math.cos(deg*Math.PI/180),y = r1*Math.sin(deg*Math.PI/180)
-                var x1 = r*Math.cos((deg+30)*Math.PI/180),y1 = r1*Math.sin((deg+30)*Math.PI/180)
-                var x2 = r*Math.cos((deg+60)*Math.PI/180),y1 = r1*Math.sin((deg+60)*Math.PI/180)
+                var x1 = r*Math.cos((deg+30)*Math.PI/180),y1 = r*Math.sin((deg+30)*Math.PI/180)
+                var x2 = r1*Math.cos((deg+60)*Math.PI/180),y2 = r1*Math.sin((deg+60)*Math.PI/180)
                 if(i == 0) {
                     context.moveTo(x,y)
                 }
@@ -57,22 +57,23 @@ export default class StarShapeImageSourceComponent extends Component {
                 context.lineTo(x2,y2)
             }
         }
-        context.globalAlpha = 1
+
         var w = image.width
         var h = image.height
         const canvas = document.createElement('canvas')
         canvas.width = w
         canvas.height = h
         const context = canvas.getContext('2d')
-        var r = canvas.width/2
+        context.globalAlpha = 1
+        var r = Math.min(w,h)/2
         var r1 = r/3
         context.save()
         context.translate(w/2,h/2)
         defineShape(context)
-        context.clipPath()
+        context.clip()
         context.drawImage(image,-w/2,-h/2)
         context.save()
-        context.restore(-w/2,0)
+        context.translate(-w/2,0)
         context.scale(scale,1)
         context.globalAlpha = 0.5
         context.fillStyle = color
